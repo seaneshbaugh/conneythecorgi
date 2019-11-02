8ba79f3010e9a8de66d5ab8ce7e4a1cf2434226e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-11-02 00:26:31 -0500

    Added materialize-css. Use webpack for handling sass.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    site: "./source/javascripts/site.js"
+    site: "./source/javascripts/site.js",
+    styles: "./source/stylesheets/site.css.scss"
   },
-  // resolve: {
-  //   root: __dirname + "/source/javascripts",
-  // },
   output: {
     path: path.join(__dirname, "dist"),
     filename: "javascripts/[name].js",
@@ -23,7 +22,21 @@ module.exports = {
             presets: ["@babel/preset-env"]
           }
         }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
